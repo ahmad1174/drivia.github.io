f142c9a1668528e60975653a243489f10076fc8c
AHMAD ZAHEER <ahmadzaheer74@gmail.com>
2020-03-31 16:05:36 +0500

    Make the content dynamic

--- script.js
+++ script.js
@@ -4,18 +4,23 @@ const base_url = 'https://www.boredapi.com/api/activity/';
 console.log(base_url);
 
 
-
-
+document.getElementById("activity_heading").style.visibility='hidden';
+document.getElementById("type_heading").style.visibility='hidden';
+document.getElementsByClassName("participants_text")[0].style.visibility='hidden';
+function fetchData(){
 fetch(base_url).then(response => response.json()).then(res => {
-    console.log(res);
+   console.log(res);
     console.log(res.activity);
     console.log(res.type);
     console.log(res.participants);
+    document.getElementById("activity_heading").style.visibility='visible';
+document.getElementById("type_heading").style.visibility='visible';
+document.getElementsByClassName("participants_text")[0].style.visibility='visible';
     document.getElementById("activity_text").innerHTML = res.activity;
     document.getElementById("type_text").innerHTML = res.type;
-    document.getElementById("participants_text").innerHTML = res.participants;
+    document.getElementsByClassName("participants_text")[1].innerHTML = res.participants;
 });
-
+}
 
 
 
