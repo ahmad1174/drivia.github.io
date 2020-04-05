5bd9473df2c307cb5caea95a062e983f33bc821a
AHMAD ZAHEER <ahmadzaheer74@gmail.com>
2020-04-05 15:48:34 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
 
+document.getElementById("activity_heading").style.visibility='hidden';
+document.getElementById("type_heading").style.visibility='hidden';
+document.getElementsByClassName("participants_text")[0].style.visibility='hidden';
 const base_url = 'https://www.boredapi.com/api/activity/';
 console.log(base_url);
 
 
-document.getElementById("activity_heading").style.visibility='hidden';
-document.getElementById("type_heading").style.visibility='hidden';
-document.getElementsByClassName("participants_text")[0].style.visibility='hidden';
+
 function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
