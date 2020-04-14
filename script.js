c636a0ff1445a12446882671b4c35a127b4d8e86
AHMAD ZAHEER <ahmadzaheer74@gmail.com>
2020-04-14 18:39:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,11 @@ document.getElementsByClassName("participants_text")[0].style.visibility='visibl
     document.getElementsByClassName("participants_text")[1].innerHTML = res.participants;
 });
 };
+self.addEventListener('beforepromptinstall', e =>{
+
+console.log("Prompt event called!",e);
+
+});
 
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
