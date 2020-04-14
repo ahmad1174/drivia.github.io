73542eb8b2625d8658619ceb6d3a9df49ce3479b
AHMAD ZAHEER <ahmadzaheer74@gmail.com>
2020-04-14 17:34:57 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
+self.addEventListener('install', event => {
 
+    function onInstall () {
+      return caches.open('static')
+        .then(cache => cache.addAll([
+          '/images/drivia-192.png',
+          '/images/drivia-512.png',
+          '/images/drivia.png',
+          '/images/drivia-favicon.png',
+          '/script.js',
+          '/styles.css',
+          '/index.html',
+          '/'
+        ])
+      );
+    }
+  
+    event.waitUntil(onInstall(event));
+  });
+
+  self.addEventListener('fetch', function(event) {
+    console.log(event);
+   
+   });
