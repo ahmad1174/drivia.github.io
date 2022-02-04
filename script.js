57bcecb4e287e899c25f150a53e1485e7c21541f
AHMAD ZAHEER <ahmadzaheer74@gmail.com>
2022-02-04 17:04:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ console.log(base_url);
 
 const offline_response = [];
 
-for (let i=0; i<1; i++){
+for (let i=0; i<3; i++){
 console.log('runing');
   fetch(base_url).then(resi => resi.json).then(resp => {
     console.log(resp);
