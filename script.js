2a5b6e8829584140320cfabcb1ad479fde7d8878
AHMAD ZAHEER <ahmadzaheer74@gmail.com>
2022-03-03 18:02:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,6 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
   };
-function fetchData(){
-fetch(base_url).then(response => response.json()).then(res => {
-  console.log(res);
-    document.getElementById("activity_heading").style.visibility='visible';
-document.getElementById("type_heading").style.visibility='visible';
-document.getElementsByClassName("participants_text")[0].style.visibility='visible';
-    document.getElementById("activity_text").innerHTML = res.activity;
-    document.getElementById("type_text").innerHTML = res.type;
-    document.getElementsByClassName("participants_text")[1].innerHTML = res.participants;
-});
-};
+
 
 
